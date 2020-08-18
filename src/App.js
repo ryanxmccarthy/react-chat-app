95961f5be91e3c477ea6aa31289365ffd0e94cf8
Ryan XVX <ryanxmccarthy@gmail.com>
2020-08-18 09:31:35 -0700

    setting up websocket server

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 import React from "react";
-import logo from "./logo.svg";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import { connect } from "react-redux";
+import * as ChatActions from "./store/actions/chatActions";
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route
-            path="/login"
-            render={(props) => {
-              return <h1>Login</h1>;
-            }}
-          ></Route>
+class App extends React.Component {
+  componentDidMount() {
+    this.props.setupSocket();
+  }
 
-          <Route
-            path="/"
-            render={(props) => {
-              return <h1>Root</h1>;
-            }}
-          ></Route>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Route
+              path="/login"
+              render={(props) => {
+                return <h1>Login</h1>;
+              }}
+            ></Route>
+
+            <Route
+              path="/"
+              render={(props) => {
+                return <h1>Root</h1>;
+              }}
+            ></Route>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
   ...state.auth,
+  ...state.chat,
 });
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  setupSocket: () => {
+    dispatch(ChatActions.setupSocket());
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
