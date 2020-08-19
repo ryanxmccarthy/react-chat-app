c62942fcaa5dc7c265256f3d55b6a7b47e93e51b
Ryan XVX <ryanxmccarthy@gmail.com>
2020-08-19 10:22:51 -0700

    working on login authorization

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import * as ChatActions from "./store/actions/chatActions";
+import * as AuthActions from "./store/actions/authActions";
 import Auth from "./components/pages/Auth";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/swag.css";
@@ -14,11 +15,36 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <button
+          onClick={(e) => {
+            this.props.logout();
+          }}
+        >
+          Log Out
+        </button>
         <BrowserRouter>
           <Switch>
-            <Route path="/login" component={Auth}></Route>
+            <Route
+              path="/login"
+              render={(props) => {
+                if (this.props.token) {
+                  return <Redirect to="/" />;
+                } else {
+                  return <Auth />;
+                }
+              }}
+            />
 
-            <Route path="/signup" component={Auth}></Route>
+            <Route
+              path="/signup"
+              render={(props) => {
+                if (this.props.token) {
+                  return <Redirect to="/" />;
+                } else {
+                  return <Auth />;
+                }
+              }}
+            ></Route>
 
             <Route
               path="/"
@@ -45,6 +71,9 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => ({
   setupSocket: () => {
     dispatch(ChatActions.setupSocket());
+  },
+  logout: () => {
+    dispatch(AuthActions.logout());
   },
 });
 
--- src/store/actions/chatActions.js
+++ src/store/actions/chatActions.js
@@ -1,3 +1,5 @@
+import * as AuthActions from "./authActions";
+
 export const setupSocket = () => {
   return (dispatch) => {
     const socket = new WebSocket("ws://localhost:3030");
@@ -6,6 +8,16 @@ export const setupSocket = () => {
         type: "SETUP_SOCKET",
         payload: socket,
       });
+    };
+
+    socket.onmessage = (message) => {
+      console.log("message", message);
+      let data = JSON.parse(message);
+      switch (data.type) {
+        case "LOGGEDIN":
+          dispatch(AuthActions.loggedIn(data));
+        default:
+      }
     };
   };
 };
